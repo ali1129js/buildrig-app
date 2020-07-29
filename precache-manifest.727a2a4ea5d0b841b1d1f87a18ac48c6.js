ac5c46f1f8427b119585b053a96a7bda2db0fc2b
ali1129js <ali1129js@gmail.com>
2020-07-29 12:48:47 +0200

    Updates

---
+++ precache-manifest.727a2a4ea5d0b841b1d1f87a18ac48c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdc182023d228974062b64d741d5cd7",
+    "url": "/buildrig-app/index.html"
+  },
+  {
+    "revision": "381d9e190f246db2231a",
+    "url": "/buildrig-app/static/css/main.937908dc.chunk.css"
+  },
+  {
+    "revision": "e11385051f741095447f",
+    "url": "/buildrig-app/static/js/2.cb599b44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buildrig-app/static/js/2.cb599b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381d9e190f246db2231a",
+    "url": "/buildrig-app/static/js/main.05558139.chunk.js"
+  },
+  {
+    "revision": "25cbd0025c4789c091e1",
+    "url": "/buildrig-app/static/js/runtime-main.8acd1a05.js"
+  }
+]);
